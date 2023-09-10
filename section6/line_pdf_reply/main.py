46189abe7517e34293aa45ac82ad3e43eabfdf72
nyanta012 <112705961+nyanta012@users.noreply.github.com>
2023-09-10 14:47:13 +0900

    Create main.py

---
+++ section6/line_pdf_reply/main.py
@@ -0,0 +1,173 @@
+import json
+import os
+import tempfile
+from datetime import datetime
+
+from flask import Request, abort
+from google.cloud import firestore, storage
+from langchain.chat_models import ChatOpenAI
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from llama_index import (
+    LLMPredictor,
+    QuestionAnswerPrompt,
+    ServiceContext,
+    StorageContext,
+    load_index_from_storage,
+)
+from llama_index.indices.base import BaseIndex
+
+FILES_TO_DOWNLOAD = ["docstore.json", "index_store.json", "vector_store.json"]
+BUCKET_NAME = "udemy-vector-store"
+
+line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
+handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
+db = firestore.Client()
+
+USE_HISTORY = True
+
+
+def download_files_from_storage(temp_dir: str) -> None:
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(BUCKET_NAME)
+
+    for file in FILES_TO_DOWNLOAD:
+        blob = bucket.blob(file)
+        blob.download_to_filename(f"{temp_dir}/{file}")
+
+
+def setup_storage_and_load_index() -> BaseIndex:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        download_files_from_storage(temp_dir)
+        service_context = ServiceContext.from_defaults(
+            llm_predictor=LLMPredictor(
+                llm=ChatOpenAI(
+                    temperature=0, model_name="gpt-3.5-turbo-0613", max_tokens=512
+                )
+            )
+        )
+        storage_context = StorageContext.from_defaults(persist_dir=temp_dir)
+        return load_index_from_storage(
+            storage_context=storage_context, service_context=service_context
+        )
+
+
+index = setup_storage_and_load_index()
+
+
+def get_previous_messages(user_id: str) -> list:
+    query = (
+        db.collection("users")
+        .document(user_id)
+        .collection("messages")
+        .order_by("timestamp", direction=firestore.Query.DESCENDING)
+        .limit(3)
+    )
+    return [
+        {
+            "chatgpt_response": doc.to_dict()["chatgpt_response"],
+            "user_message": doc.to_dict()["user_message"],
+        }
+        for doc in query.stream()
+    ]
+
+
+def format_history(previous_messages: list) -> str:
+    return "".join(
+        f"ユーザー: {d['user_message']}\nアシスタント: {d['chatgpt_response']}\n"
+        for d in previous_messages[::-1]
+    )
+
+
+def generate_response(user_message: str, history: str) -> str:
+    if USE_HISTORY:
+        PROMPT = """
+        あなたは親切なアシスタントです。
+        これまでのユーザーとアシスタントの会話の履歴は以下のようになっています。
+
+        ---------------------
+        {history}
+        ---------------------
+
+        また、以下に文献の情報を提供します。
+
+        ---------------------
+        {{context_str}}
+        ---------------------
+
+        与えられた情報を元にユーザーへのアドバイスを200文字以内で出力してください。
+        文献の情報から回答できない入力の場合は、そのように出力してください。
+
+        入力：{{query_str}}
+
+        出力：
+        """.format(
+            history=history
+        )
+    else:
+        PROMPT = """
+        あなたは親切なアシスタントです。
+        以下に文献の情報を提供します。
+
+        ---------------------
+        {{context_str}}
+        ---------------------
+
+        与えられた情報を元にユーザーへのアドバイスを200文字以内で出力してください。
+        文献の情報から回答できない入力の場合は、そのように出力してください。
+
+        入力：{{query_str}}
+
+        出力：
+        """.format()
+
+    query_engine = index.as_query_engine(text_qa_template=QuestionAnswerPrompt(PROMPT))
+    return str(query_engine.query(user_message))
+
+
+def reply_to_user(reply_token: str, chatgpt_response: str) -> None:
+    line_bot_api.reply_message(reply_token, TextSendMessage(text=chatgpt_response))
+
+
+def save_message_to_db(user_id: str, user_message: str, chatgpt_response: str) -> None:
+    doc_ref = db.collection("users").document(user_id).collection("messages").document()
+    doc_ref.set(
+        {
+            "user_message": user_message,
+            "chatgpt_response": chatgpt_response,
+            "timestamp": datetime.now(),
+        }
+    )
+
+
+###################
+
+
+def main(request: Request) -> str:
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return "OK"
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event: MessageEvent) -> None:
+    user_id = event.source.user_id
+    user_message = event.message.text
+
+    if USE_HISTORY:
+        previous_messages = get_previous_messages(user_id)
+        history = format_history(previous_messages)
+    else:
+        history = None
+    chatgpt_response = generate_response(user_message, history)
+
+    reply_to_user(event.reply_token, chatgpt_response)
+    if USE_HISTORY:
+        save_message_to_db(user_id, user_message, chatgpt_response)
